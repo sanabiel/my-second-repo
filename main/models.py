cb563704b2757666e6fc99268c3dc2166912cc9e
sanabiel <nabiel.ahmad@ui.ac.id>
2023-09-20 11:31:29 +0700

    perbaikan date

--- main/models.py
+++ main/models.py
@@ -5,5 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
 
     
